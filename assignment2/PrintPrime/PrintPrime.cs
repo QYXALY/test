b61b59004976bd27732f924b9bbd9133d4290fb9
DYX <715373873@qq.com>
2023-03-07 20:29:02 +0800

    ass2

---
+++ assignment2/PrintPrime/PrintPrime.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace project1
+{
+    class project1
+    {
+        static bool isPrime(int a) //判断素数
+        {
+            bool flag = true;
+            for(int i=2;i<a;i++)
+            {
+                if(a%i==0)
+                    flag = false;
+            }
+
+            return flag;
+        }
+        static void Main(string[]args)
+        {
+            Console.WriteLine("请输入一个整数");
+            int a=Int32.Parse(Console.ReadLine());
+            for(int i=2;i<=a;i++)
+            {
+                if(a%i==0&&isPrime(i))
+                    Console.Write(i+" ");
+            }
+        }
+    }
+}
---
+++ assignment2/SieveOfEratosthenes/SieveOfEratosthenes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace project3
+{
+    class project3
+    {
+        static void Main(string[]args)
+        {
+            int[] arr = new int[99];
+            for(int i = 0; i < arr.Length; i++)//初始化数组
+            {
+                arr[i] = i+2;
+            }
+
+            for(int i = 2; i <=100; i++)
+            {
+                for(int j = 0; j < arr.Length; j++)
+                {
+                    if (arr[j] % i == 0 && arr[j]!=i)
+                        arr[j] = 0;
+                }
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i]!=0)
+                {
+                    Console.Write(arr[i]+" ");
+                }
+            }
+        }
+    }
+}
