e8ff3d1ed5b18a3c695f31885c532f919940cf3d
DYX <715373873@qq.com>
2023-03-07 20:27:53 +0800

    123

---
+++ assignment1/Calculator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace calculater
+{
+    class cal
+    {
+        static void Main(string[]args)
+        {
+            double a;
+            double b;
+            char c;
+            Console.WriteLine("请输入第一个数");
+            a = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("请输入第二个数");
+            b = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("请输入运算符号");
+            c =Char.Parse(Console.ReadLine());
+
+            switch (c)
+            {
+                case '+':
+                    Console.WriteLine(a + b);
+                    break;
+                case '-':
+                    Console.WriteLine(a - b);
+                    break;
+                case '*':
+                    Console.WriteLine(a * b);
+                    break;
+                case '/':
+                    Console.WriteLine(a / b);
+                    break;
+                default:
+                    Console.WriteLine("输入错误");
+                    break;
+            }
+        }
+
+    }
+
+}
